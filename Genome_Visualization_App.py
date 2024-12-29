8dc2ea40ecb9dc60519a0f412a71fcaa022fb317
Adam Hetherwick <122589031+AJHetherwick@users.noreply.github.com>
2024-12-29 12:12:23 -0800

    Update Genome_Visualization_App.py

--- Genome_Visualization_App.py
+++ Genome_Visualization_App.py
@@ -91,7 +91,7 @@ def main() -> None:
     st.session_state.setdefault("past_gene_ids", [])
 
     # Dropdown to show past searches
-    st.text("Gene IDs from past searches:", st.session_state.past_gene_ids)
+    st.text(f"Gene IDs from past searches: {st.session_state.past_gene_ids}")
 
     # User input for multiple gene IDs
     gene_id_input = st.text_input('Enter Gene IDs (space-separated) to visualize individually.')
