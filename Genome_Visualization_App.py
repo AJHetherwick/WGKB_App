1a7b9d223e2ed62cf586f4ddc7db6b9bed4ffd58
Adam Hetherwick <122589031+AJHetherwick@users.noreply.github.com>
2025-01-03 16:59:19 -0800

    Update example plot links

--- Genome_Visualization_App.py
+++ Genome_Visualization_App.py
@@ -322,9 +322,9 @@ def fetch_species_suggestions(query):
 def show_example_plots() -> None:
     st.markdown("#### Example Plots")
     example_plot_urls = [
-        "https://github.com/AJHetherwick/WGKB_App/blob/9b5bdbadcae8e9ebcc9d4a284820d0ed19fbe9fb/juglans_regia_plot.png",
-        "https://github.com/AJHetherwick/WGKB_App/blob/9b5bdbadcae8e9ebcc9d4a284820d0ed19fbe9fb/juglans_microcarpa_plot.png",
-        "https://github.com/AJHetherwick/WGKB_App/blob/9b5bdbadcae8e9ebcc9d4a284820d0ed19fbe9fb/juglans_microcarpa_rem_genes.png"
+        "https://raw.githubusercontent.com/AJHetherwick/WGKB_App/9b5bdbadcae8e9ebcc9d4a284820d0ed19fbe9fb/juglans_regia_plot.png",
+        "https://raw.githubusercontent.com/AJHetherwick/WGKB_App/9b5bdbadcae8e9ebcc9d4a284820d0ed19fbe9fb/juglans_microcarpa_plot.png",
+        "https://raw.githubusercontent.com/AJHetherwick/WGKB_App/9b5bdbadcae8e9ebcc9d4a284820d0ed19fbe9fb/juglans_microcarpa_rem_genes.png"
     ]
 
     for url in example_plot_urls:
